3b65de4adb60483f368409f66c8740a7a390ec5d
Asahel Enock Kigen <kirwaasahel@gmail.com>
2022-01-24 22:24:33 +0300

    third commit

---
+++ app/src/main/java/com/example/kotlinintro/TwitterCloneActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TwitterCloneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitter_clone)
+    }
+}
